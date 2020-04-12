8e7424c47f5424c0c7ef7f07dd7f9c9f9076ede6
Sean Toru <sean@toruinteractive.com>
2020-04-12 23:24:21 +0100

    Table

--- docs/streets.js
+++ docs/streets.js
@@ -1,41 +1,29 @@
 'use strict'
 
-var data_url = "https://digital-northampton.github.io/volunteer-streets/codes.json"
+var data_url = "https://digital-northampton.github.io/volunteer-streets/streets.json"
 
 $ (document).ready (function () {
-  var $table = $("table#codes")
+  var $table = $("table#streets")
   var $button = $("button#download")
 
   var numberWithCommas = function (x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
   }
 
-  var addRow = function (code, rowData) {
+  var addRow = function (street) {
     var html = ""
-    var hpv
-
-    if (rowData.volunteers == 0) {
-      hpv = "No Volunteers"
-    } else {
-      hpv = Math.round (rowData.households/rowData.volunteers)
-    }
 
     html += "<tr>"
-    html += "<td><h4>"+code+"</h4></td>"
-    html += "<td>"+rowData.areas.sort().join(", ")+"</td>"
-    html += "<td class='right'>"+numberWithCommas (rowData.households)+"</td>"
-    html += "<td class='right'>"+numberWithCommas (rowData.population)+"</td>"
-    html += "<td class='right'>"+numberWithCommas (rowData.volunteers)+"</td>"
-    html += "<td class='right'>"+numberWithCommas (hpv)+"</td>"
-    html += "/<tr>"
+    html += "<td>"+street.name+"</td>"
+    html += "</tr>"
 
     $table.find ("tbody").append (html)
   }
 
-  $.getJSON (data_url, function (data) {
-    for (var code in data) {
-      addRow (code, data[code])
-    }
+  $.getJSON (data_url, function (data) {    
+    data.forEach (street => {
+      addRow (street)
+    })
   })
 
   $button.bind ("click", function () {
