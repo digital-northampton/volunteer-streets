02e2946bf700a8facc030c4d77f244b20cd1dd17
Sean Toru <sean@toruinteractive.com>
2020-04-12 10:18:35 +0100

    load codes

--- process-county-postcodes.js
+++ process-county-postcodes.js
@@ -3,9 +3,22 @@
 const neatCsv = require ('neat-csv')
 const fs = require ('fs')
 
+const countycodes_file_path = "data/nn-postcodes.csv"
+
+let countycodes
+
 const loadCountyPostcodes = () => {
   return new Promise ((resolve, reject) => {
-    resolve ()
+    fs.readFile (countycodes_file_path, (error, data) => {
+        if (error) {
+          reject (error);
+        } else {
+          (async () => {
+            countycodes = await neatCsv (data);
+            resolve ();
+          })();
+        }
+    });
   })
 }
 
