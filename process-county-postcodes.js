d17ea23c377fa751a479334962e1f2b762b2bd02
Sean Toru <sean@toruinteractive.com>
2020-04-12 10:21:02 +0100

    Load volunteer JSON

--- process-county-postcodes.js
+++ process-county-postcodes.js
@@ -4,7 +4,9 @@ const neatCsv = require ('neat-csv')
 const fs = require ('fs')
 
 const countycodes_file_path = "data/nn-postcodes.csv"
+const volunteers_path = "data/volunteers.json"
 
+let volunteers
 let countycodes
 
 const loadCountyPostcodes = () => {
@@ -22,6 +24,19 @@ const loadCountyPostcodes = () => {
   })
 }
 
+const loadVolunteers = () => {
+  return new Promise ((resolve, reject) => {
+    fs.readFile (volunteers_path, (error, data) => {
+        if (error) {
+          reject (error);
+        } else {
+          volunteers = JSON.parse (data)
+        }
+    });
+  })
+}
+
 loadCountyPostcodes ()
+  .then (loadVolunteers)
   .then (() => console.log ("🔥"))
   .catch (e => console.log (e))
