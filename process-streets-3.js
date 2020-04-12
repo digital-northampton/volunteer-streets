72bf67a405808391822be149c009c971b8e2fb55
Sean Toru <sean@toruinteractive.com>
2020-04-12 15:53:40 +0100

    recursive

--- process-streets-3.js
+++ process-streets-3.js
@@ -4,33 +4,64 @@ const fs = require ('fs')
 const path = require ('path')
 const https = require ('https')
 
+const street_data_dir = "data/streets"
 const request_url = "https://www.overpass-api.de/api/interpreter"
 const request_data = "[out:json];way(ID);(._;>;);out body;"
 
+let ids
+let index = 0
+
+const getIDs = () => {
+  return new Promise ((resolve, reject) => {
+    const files = fs.readdirSync (street_data_dir)
+
+    ids = files
+            .filter (f => f.indexOf (".json") > -1)
+            .map (f => f.replace (".json", ""))
+            .map (f => parseInt (f))
+    
+    //tmp
+    ids = ids.slice (0, 4)
+
+    resolve ()
+  })
+}
+
 const getStreetNodes = () => {
   return new Promise ((resolve, reject) => {
-    const id = 100017851
+    const id = ids[index]
     const url = request_url + "?data=" + request_data.replace ("ID", id)
-    
+
     https.get (url, (resp) => {
       let data = '';
 
       resp.on('data', (chunk) => {
         data += chunk;
       });
+
       resp.on('end', () => {
-        console.log(JSON.parse(data));
-        resolve ()
+        const percent = Math.round (100*index/ids.length)
+        process.stdout.clearLine();
+        process.stdout.cursorTo(0);
+        process.stdout.write(percent + '% Streets');
+        
+        index++
+        if (ids [index] == undefined) {
+          resolve ()
+          return;
+        }
+
+        getStreetNodes ()
+          .catch (() => reject ())
       });
     }).on("error", (err) => {
       reject ("Error: " + err.message);
     });
-
-    resolve ()
   })
 }
 
-getStreetNodes ()
+getIDs ()
+  .then (getStreetNodes)
   .then (() => console.log ("🔥"))
   .catch (e => console.log (e))
 
