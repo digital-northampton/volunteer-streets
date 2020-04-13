55779cbdf75bb6d3fa8c5925100ccca84dadf6e6
Sean Toru <sean@toruinteractive.com>
2020-04-13 19:11:24 +0100

    Data

--- process-streets-4.js
+++ process-streets-4.js
@@ -3,11 +3,23 @@
 const fs = require ('fs')
 const path = require ('path')
 
-const street_data_dir = "data/streets/"
-const volunteer_data_path = "data/volunteers.json"
+const street_data_dir = "docs/streets/"
+const volunteer_data_path = "docs/volunteers.json"
 
 let ids
 let volunteers
+
+const measure = (lat1, lon1, lat2, lon2) => {
+  var R = 6378.137; // Radius of earth in KM
+  var dLat = lat2 * Math.PI / 180 - lat1 * Math.PI / 180;
+  var dLon = lon2 * Math.PI / 180 - lon1 * Math.PI / 180;
+  var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+  Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) *
+  Math.sin(dLon/2) * Math.sin(dLon/2);
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+  var d = R * c;
+  return d * 1000; // meters
+}
 
 const getIDs = () => {
   return new Promise ((resolve, reject) => {
@@ -17,55 +29,54 @@ const getIDs = () => {
             .filter (f => f.indexOf (".json") > -1)
             .map (f => f.replace (".json", ""))
             .map (f => parseInt (f))
-   	
+    
     resolve ()
   })
 }
 
 const loadVolunteers = () => {
-	return new Promise ((resolve, reject) => {
-  	const rawdata = fs.readFileSync (volunteer_data_path)
-  	volunteers = JSON.parse (rawdata)
-  	resolve ()
-	})
+  return new Promise ((resolve, reject) => {
+    const rawdata = fs.readFileSync (volunteer_data_path)
+    volunteers = JSON.parse (rawdata)
+    resolve ()
+  })
 }
 
 const setPostcodes = () => {
   return new Promise ((resolve, reject) => {
 
-  	ids.forEach ((id, index) => {
-	  	const filename = street_data_dir + id + ".json"
-	  	const rawdata = fs.readFileSync (filename)
-	  	const street = JSON.parse (rawdata)
-	  	
-	  	let closest_distance = 99999999999999;
-	  	let closest_volunteer_postcode = "";
+    ids.forEach ((id, index) => {
+      const filename = street_data_dir + id + ".json"
+      const rawdata = fs.readFileSync (filename)
+      const street = JSON.parse (rawdata)
+      
+      let closest_distance = 99999999999999;
+      let closest_volunteer_postcode = "";
 
-	  	street.nodes.forEach (n => {
-		  	volunteers.forEach (v => {
-		  		const a = parseFloat (v.lat) - parseFloat (n.lon)
-					const b = parseFloat (v.lng) - parseFloat (n.lat)
-					const c = Math.sqrt (a * a + b * b)
+      street.nodes.forEach (n => {
+        volunteers.forEach (v => {
+          const m = Math.round (measure (v.lat, v.lng, n.lon, n.lat))
+          // note n coords are wrong way round
+          if (m < closest_distance) {
+            closest_volunteer_postcode = v.postcode
+            closest_distance = m
+          }
+        })
+      })
 
-					if (c < closest_distance) {
-						closest_volunteer_postcode = v.postcode
-						closest_distance = c
-					}
-		  	})
-	  	})
+      street.volunteer_postcode = closest_volunteer_postcode
+      street.closest_distance = closest_distance
 
-	  	street.volunteer_postcode = closest_volunteer_postcode
-
-	  	const data = JSON.stringify (street)
+      const data = JSON.stringify (street)
       fs.writeFileSync (filename, data);
-  	})
+    })
 
-  	resolve ()
+    resolve ()
   })
 }
 
 getIDs ()
-	.then (loadVolunteers)
-	.then (setPostcodes)
+  .then (loadVolunteers)
+  .then (setPostcodes)
   .then (() => console.log ("🔥"))
   .catch (e => console.log (e))
